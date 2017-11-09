2dea44faf12c82149a50f921360bf63e87423666
Cara Kane <cara.kane@gmail.com>
2017-11-09 16:31:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 function printBadges(array) {
   for(let i=0; i<array.length; i++) {
-    console.log(`Welcome ${array[i]}! You are employee #${i}.`)
-    
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`)
+
   }
   return array
 }
