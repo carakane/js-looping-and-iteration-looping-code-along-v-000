f98446bb29b46c90996d094a0f7904dc69fd17a7
Cara Kane <cara.kane@gmail.com>
2017-11-09 16:30:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Code your solutions in this file
 
 function printBadges(array) {
-  for(i=0; i<array.length; i++) {
-    console.log(array[i])
+  for(let i=0; i<array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${i}.`)
+    
   }
+  return array
 }
 
 function tailsNeverFails() {
-  
+
 }
